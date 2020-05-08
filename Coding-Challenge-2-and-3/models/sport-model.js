c59a61d68253e0ff3430c495309ab93e6b5e4a4d
Martin Ruiz <martin.ruiz.mares@gmail.com>
2020-05-08 09:37:49 -0500

    Second Partial Exam

--- Coding-Challenge-2-and-3/models/sport-model.js
+++ Coding-Challenge-2-and-3/models/sport-model.js
@@ -1,8 +1,51 @@
 const mongoose = require( 'mongoose' );
 const uuid = require( 'uuid' );
 
-/* Your code goes here */
+const sportsSchema = mongoose.Schema({
+    id: {
+        type: String,
+        default: uuid.v4(),
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    num_players: {
+        type: Number,
+        require: true
+    }
+
+})
+
+const sportsCollection = mongoose.model('sports', sportsSchema)
+
+const Sports = {
+    create: function(sport){
+        return sportsCollection
+            .create(sport)
+            .then(result => {
+                return result
+            })
+            .catch (err => {
+                return error
+            })
+    },
+    delete: function(sport){
+        return sportsCollection
+            .deleteOne(sportsCollection.find({id: sport}))
+            .then(response => {
+                if(response.n > 0)
+                    return true
+                else
+                    return false
+            })
+            .catch (err => {
+                return error
+            })
+    }
+}
 
 module.exports = {
-    
+    Sports
 };
--- Coding-Challenge-2-and-3/server.js
+++ Coding-Challenge-2-and-3/server.js
@@ -2,12 +2,98 @@ const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const mongoose = require( 'mongoose' );
 const jsonParser = bodyParser.json();
+const morgan = require('morgan')
+const { Sports} = require('./models/sport-model')
 const { DATABASE_URL, PORT } = require( './config' );
+const uuid = require( 'uuid' );
 
 const app = express();
+app.use(morgan('dev'))
+app.use(jsonParser)
 
+const sports = [
+    {
+        id: 2
+    },
+    {
+        id: 3
+    }
+]
 
-/* Your code goes here */
+app.post('/sports', (req, res) => {
+    Sports.create({
+        id: uuid.v4(), 
+        name: "Test",
+        num_players: 123
+    })
+    .then(result => {
+        return res.status(201).json(result)
+    })
+})
+
+///// Third Challenge
+app.delete('/sports/delete', (req, res) => {
+    let {id} = req.body
+    if(!id){
+        res.statusMessage = "ID not found in body of the request"
+        res.status(406).end()
+    }
+
+    let {sportId} = req.query
+    if(!sportId){
+        res.statusMessage = "ID not found in query params of the request"
+        res.status(406).end()
+    }
+
+    if(id !== sportId){
+        res.statusMessage = "ID's doesn't match"
+        res.status(409).end()
+    }
+
+    Sports.delete(id)
+    .then(result => {
+        if(result === true)
+        return res.status(204).end()
+        else 
+            res.statusMessage = `Sport with ${id} doesnt exist`
+            return res.status(404).end()
+    })
+    .catch(err => {
+        res.statusMessage = "Error in the database"
+        return res.status(500).end()
+    })
+
+})
+
+///// Second Challenge
+// app.delete('/sports/delete', (req, res) => {
+//     let {id} = req.body
+//     if(!id){
+//         res.statusMessage = "ID not found in body of the request"
+//         res.status(406).end()
+//     }
+
+//     let {sportId} = req.query
+//     if(!sportId){
+//         res.statusMessage = "ID not found in query params of the request"
+//         res.status(406).end()
+//     }
+
+//     if(id !== sportId){
+//         res.statusMessage = "ID's doesn't match"
+//         res.status(409).end()
+//     }
+
+//     sports.find((sport) => {
+//         if(sport.id === id){
+//             return res.status(204).end()
+//         }
+//     })
+
+//     res.statusMessage = `Sport with ${id} doesnt exist`
+//     return res.status(404).end()
+// })
+
 
 
 app.listen( PORT, () => {
